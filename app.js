98313308341e745f3e445b17aacb17d5b02ca9db
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-07 13:57:19 -0800

    App mostly funcitonal. Just need to fix register state not being passed down

--- app.js
+++ app.js
@@ -61,32 +61,53 @@ app.get('/profile/:id', (req, res) => {
 
 });
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error loggin in');
-    }
+    db.select('email', 'hash')
+        .from('login')
+        .where('email', '=', req.body.email)
+        .then((data) => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid) {
+                return db.select('*')
+                    .from('users')
+                    .where('email', '=', req.body.email)
+                    .then((user) => {
+                        console.log(user)
+                        res.json(user[0]);
+                    })
+                    .catch((err) => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 });
 app.post('/register', (req, res) => { 
     const {name, email, password} = req.body;
     // Hashing password
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .then(user => {
-            res.json(user[0]);
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
         })
-        .catch(err => 
-            res.status(400)
-            .json('unable to login')
-        );
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 });
 app.put('/image', (req, res) => {
     const { id } = req.body;
@@ -94,7 +115,7 @@ app.put('/image', (req, res) => {
         .increment('entries', 1)
         .returning('entries')
         .then(entries => {
-            console.log(entries);
+            res.json(entries[0].entries);
         })
 });
 // Server on
