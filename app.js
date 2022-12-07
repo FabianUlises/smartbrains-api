efa41697bf639aac8c29fe982f48e7f7b43b3f62
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-07 03:45:36 -0800

    Added signin post route

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+// Dependencies
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware
+app.use(bodyParser.json());
+// mockdata
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
     res.send(' home stub route')
+});
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error loggin in');
+    }
 });
 app.listen(3001, (err) =>{
     console.log('server listening');
