e38a1a2004e50d0db832ec3320dc22bcdcb5a7cd
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-07 07:42:05 -0800

    Connected back end to front end

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 // Middleware
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(bodyParser.json());
 // mockdata
 const database = {
@@ -22,13 +26,26 @@ const database = {
             email: 'sally@gmail.com',
             password: 'bananas',
             entries: 0,
-            joined: new Date()
+            joined: new Date() 
         }
     ]
 }
-
+// Routes
 app.get('/', (req, res) => {
-    res.send(' home stub route')
+    res.json(database.users);
+});
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found) {
+        res.status(400).json('user not found');
+    }
 });
 app.post('/signin', (req, res) => {
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
@@ -37,8 +54,12 @@ app.post('/signin', (req, res) => {
         res.status(400).json('error loggin in');
     }
 });
-app.post('/register', (req, res) => {
+app.post('/register', (req, res) => { 
     const {name, email, password} = req.body;
+    // Hashing password
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '224',
         name: name,
@@ -49,6 +70,22 @@ app.post('/register', (req, res) => {
     });
     res.json(database.users[database.users.length-1]);
 });
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if(!found) {
+        res.status(400).json('user not found');
+    }
+});
+// Server on
 app.listen(3001, (err) =>{
+    if(err) return console.log('error', err);
     console.log('server listening');
 });
